1c4de7869773f1cbb6e33cd6f1c08623f33777e5
Aluno-AEDS <Aluno-AEDS@DESKTOP-OHCCO70>
2023-11-22 14:07:20 +0000

    Add project files.

---
+++ WinFormsApp9/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            formatarToolStripMenuItem = new ToolStripMenuItem();
+            corETipoDeLetraToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { formatarToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // formatarToolStripMenuItem
+            // 
+            formatarToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { corETipoDeLetraToolStripMenuItem });
+            formatarToolStripMenuItem.Name = "formatarToolStripMenuItem";
+            formatarToolStripMenuItem.Size = new Size(67, 20);
+            formatarToolStripMenuItem.Text = "Formatar";
+            // 
+            // corETipoDeLetraToolStripMenuItem
+            // 
+            corETipoDeLetraToolStripMenuItem.Name = "corETipoDeLetraToolStripMenuItem";
+            corETipoDeLetraToolStripMenuItem.Size = new Size(168, 22);
+            corETipoDeLetraToolStripMenuItem.Text = "Cor e tipo de letra";
+            corETipoDeLetraToolStripMenuItem.Click += corETipoDeLetraToolStripMenuItem_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 111);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Poo em C#";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "asA";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem formatarToolStripMenuItem;
+        private ToolStripMenuItem corETipoDeLetraToolStripMenuItem;
+        private Label label1;
+    }
+}
---
+++ WinFormsApp9/Form1.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualBasic;
+
+namespace WinFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void corETipoDeLetraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+        }
+    }
+}
